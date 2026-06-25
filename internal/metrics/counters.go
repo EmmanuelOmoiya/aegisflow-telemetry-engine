7c6e991e2f8e85ad1c86dcf13eb503a8ecc9c8b6
Emmanuel Omoiya <emmanuelomoiya6@gmail.com>
2026-06-25 18:49:07 +0100

    feat: implement lock-free thread-safe atomic metric counters

---
+++ internal/metrics/counters.go
@@ -0,0 +1,33 @@
+package metrics
+
+import (
+	"sync/atomic"
+)
+
+type OperationalMetrics struct {
+	IngestedCount uint64
+	ProcessedCount	uint64
+	ViolationCount	uint64
+}
+
+var GlobalMetrics = &OperationalMetrics{}
+
+func (m *OperationalMetrics) IncrementIngested() {
+	atomic.AddUint64(&m.IngestedCount, 1)
+}
+
+func (m *OperationalMetrics) IncrementProcessed() {
+	atomic.AddUint64(&m.ProcessedCount, 1)
+}
+
+func (m *OperationalMetrics) IncrementViolated() {
+	atomic.AddUint64(&m.ViolationCount, 1)
+}
+
+func (m *OperationalMetrics) Snapshot() map[string]uint64 {
+	return map[string]uint64{
+		"ingested_events": atomic.LoadUint64(&m.IngestedCount),
+		"processed_events": atomic.LoadUint64(&m.ProcessedCount),
+		"policy_violations": atomic.LoadUint64(&m.ViolationCount)
+	}
+}
